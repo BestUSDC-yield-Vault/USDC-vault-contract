ee305a504ea2d77f3e8aa0fe0f1cec3f6081186a
bhumi18 <bhumisadariya18@gmail.com>
2024-08-06 12:14:45 +0530

    seamless deposit/withdraw🧪

--- src/LendingManager.sol
+++ src/LendingManager.sol
@@ -5,26 +5,44 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/aave/IPool.sol";
 
 contract LendingManager {
-    function deposit(address _asset, uint256 _amount, address lendingPool) external {
+    function deposit(
+        address _asset,
+        uint256 _amount,
+        address lendingPool
+    ) external {
         IPool pool = IPool(lendingPool);
         IERC20(_asset).transferFrom(msg.sender, address(this), _amount);
         IERC20(_asset).approve(address(pool), _amount);
         pool.deposit(_asset, _amount, address(this), 0);
     }
 
-    function withdraw(address _asset, uint256 _amount, address to, address lendingPool) external returns (uint256) {
+    function withdraw(
+        address _asset,
+        uint256 _amount,
+        address to,
+        address lendingPool
+    ) external returns (uint256) {
         IPool pool = IPool(lendingPool);
-        IERC20(getATokenAddress(_asset, lendingPool)).approve(address(pool), _amount);
+        IERC20(getATokenAddress(_asset, lendingPool)).approve(
+            address(pool),
+            _amount
+        );
         return pool.withdraw(_asset, _amount, to);
     }
 
-    function getATokenAddress(address _asset, address lendingPool) public view returns (address) {
+    function getATokenAddress(
+        address _asset,
+        address lendingPool
+    ) public view returns (address) {
         IPool pool = IPool(lendingPool);
         IPool.ReserveData memory reserveData = pool.getReserveData(_asset);
         return reserveData.aTokenAddress;
     }
 
-    function getInterestRate(address _asset, address lendingPool) public view returns (uint128) {
+    function getInterestRate(
+        address _asset,
+        address lendingPool
+    ) public view returns (uint128) {
         IPool pool = IPool(lendingPool);
         IPool.ReserveData memory reserveData = pool.getReserveData(_asset);
         return reserveData.currentLiquidityRate;
--- test/LendingManager.t.sol
+++ test/LendingManager.t.sol
@@ -9,28 +9,26 @@ contract LendingManagerTest is Test {
     LendingManager lendingManager;
     IERC20 token;
     IERC20 atoken;
+    IERC20 atokenSeamless;
     uint256 amount;
     uint128 aaveInterestRate;
+    uint128 seamlessInterestRate;
 
     // Mainnet fork configuration
     address LENDING_POOL_AAVE = vm.envAddress("LENDING_POOL_AAVE");
+    address LENDING_POOL_SEAMLESS = vm.envAddress("LENDING_POOL_SEAMLESS");
     address SWAP_ROUTER = vm.envAddress("SWAP_ROUTER");
     address USDC = vm.envAddress("USDC_ADDRESS");
     address USER = vm.envAddress("USER");
 
     function setUp() public {
-        // console.log("==SET UP(LendingManager.t.sol)==");
-
         // Deploy the contract
         lendingManager = new LendingManager();
 
-        // console.log(
-        //     "Deployed LendingManager contract at: %s",
-        //     address(lendingManager)
-        // );
-
         // setting up underlying token
         token = IERC20(USDC);
+
+        //----------------------------------------------------AAVE-------------------------------------------
         // fetching aToken for underlying token
         address aTOKEN = lendingManager.getATokenAddress(
             USDC,
@@ -44,12 +42,27 @@ contract LendingManagerTest is Test {
             USDC,
             LENDING_POOL_AAVE
         );
-        console.log("AAVE INTEREST RATE", aaveInterestRate / 10 ** 25);
+        // console.log("AAVE INTEREST RATE", aaveInterestRate);
+
+        //----------------------------------------------------SEAMLESS-------------------------------------------
+        //fetching aToken of Seamless for underlying token
+        address aTOKENSeamless = lendingManager.getATokenAddress(
+            USDC,
+            LENDING_POOL_SEAMLESS
+        );
+        atokenSeamless = IERC20(aTOKENSeamless);
+
+        seamlessInterestRate = lendingManager.getInterestRate(
+            USDC,
+            LENDING_POOL_SEAMLESS
+        );
+        // console.log("SEAMLESS INTEREST RATE", seamlessInterestRate);
+
         // setting up supply/withdraw amount
         amount = 100000000; // 100 USDC
-        // console.log("Setup completed.");
     }
 
+    //----------------------------------------------------AAVE-------------------------------------------
     function testDeposit() public {
         vm.startPrank(USER);
         // Check user's TOKEN balance
@@ -127,6 +140,72 @@ contract LendingManagerTest is Test {
         assertEq(
             ausdcBalanceContract - amountToWithdraw,
             0,
+            "AUSDC balance error : withdraw"
+        );
+        vm.stopPrank();
+    }
+
+    //----------------------------------------------------SEAMLESS-------------------------------------------
+    function testDepositSeamless() public {
+        vm.startPrank(USER);
+        // Check user's TOKEN balance
+        assertGt(
+            token.balanceOf(USER),
+            0,
+            "USER does not hold the underlying token"
+        );
+
+        // Approve and supply TOKEN
+        token.approve(address(lendingManager), amount);
+        assertGe(
+            token.allowance(USER, address(lendingManager)),
+            amount,
+            "Allowance should be equal to the approved amount"
+        );
+        // supply amount to aaveInteraction
+        lendingManager.deposit(USDC, amount, LENDING_POOL_SEAMLESS);
+        // console.log(
+        //     "atoken seamless",
+        //     atokenSeamless.balanceOf(address(lendingManager))
+        // );
+        assertEq(
+            atokenSeamless.balanceOf(address(lendingManager)),
+            amount,
+            "ATOKEN balance error"
+        );
+        vm.stopPrank();
+    }
+
+    function testWithdrawHalfSeamless() public {
+        testDepositSeamless();
+        vm.startPrank(USER);
+        uint256 usdcBalanceContract = token.balanceOf(address(lendingManager));
+        uint256 ausdcBalanceContract = atokenSeamless.balanceOf(
+            address(lendingManager)
+        );
+        // console.log("usdc before", usdcBalanceContract);
+        // console.log("ausdc before", ausdcBalanceContract);
+        uint256 amountToWithdraw = 50000000;
+        lendingManager.withdraw(
+            USDC,
+            amountToWithdraw,
+            address(lendingManager),
+            LENDING_POOL_SEAMLESS
+        );
+        // console.log("usdc after", token.balanceOf(address(lendingManager)));
+        // console.log(
+        //     "ausdc after",
+        //     atokenSeamless.balanceOf(address(lendingManager))
+        // );
+        assertEq(
+            usdcBalanceContract + amountToWithdraw,
+            amountToWithdraw,
+            "USDC balance error : withdraw"
+        );
+        // sometimes atoken value comes with the difference of 0.0000001. That is why used less than or equals
+        assertLe(
+            ausdcBalanceContract - amountToWithdraw,
+            50000000,
             "AUSDC balance error : withdraw"
         );
         vm.stopPrank();
